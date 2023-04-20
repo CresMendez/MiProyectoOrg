182a6952ecb38537d78272702ec89efc5229a23d
Crescencio <cmendezara20@gmail.com>
2023-04-20 13:40:25 -0600

    Modificando redes sociales

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,21 +1,24 @@
 import "./Footer.css"
 
 const Footer = () => {
-    //backgorud se pone aqui por la ruta de public
+    //backgorud se pone aqui por la ruta de public linkedin
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a without rel="noreferrer" href='https://www.linkedin.com/in/crescencio-mendez/' target='_blank'>
+                <img src="/img/linkedinyes.png" alt='linkedin' />
+            </a>
+            <a without rel="noreferrer" href='https://www.linkedin.com/in/crescencio-mendez/' target='_blank'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a without rel="noreferrer" href='https://www.linkedin.com/in/crescencio-mendez/' target='_blank'>
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a without rel="noreferrer" href='https://www.linkedin.com/in/crescencio-mendez/' target='_blank'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Crescencio</strong>
+        <strong>Desarrollado por: <br/> Crescencio Mendez Ara</strong>
     </footer>
 }
 
